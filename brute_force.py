33ed5a95b36fedea95d6882f3301cc74690908a6
Khizar Malik <k.malik@se17.qmul.ac.uk>
2020-07-14 19:37:43 +0100

    Final code change

--- brute_force.py
+++ brute_force.py
@@ -122,36 +122,3 @@ def Main():
     
 if __name__ =="__main__":
     Main()
-    
-        
-        
-# import zipfile
-# from tqdm import tqdm
-
-# wordlist = "rockyou.txt"
-# zip_file = "secret_file.zip"
-
-# # Initialize the Zip File object
-# zip_file = zipfile.ZipFile(zip_file)
-
-# # Count the number of words in this wordlist
-# n_words = len(list(open(wordlist, "rb")))
-
-# # Print the total number of passwords 
-# print("Total passwords to test: ", n_words)
-
-# with open(wordlist, "rb") as wordlist:
-#     for word in tqdm(wordlist, total=n_words, unit="word"):
-#         try:
-#             zip_file.extractall(pwd=word.strip())
-#         except:
-#             continue
-#         else:
-#             print("[+] Password found: ",word.decode().strip())
-#             exit(0)
-# print("[!] Password not found, try other wordlist")
-
-# import argparse
-# import sys
-
-# def getOption
